1c82cede343e4389f5abc47244b4637221630d04
talhaunal99 <65777560+talhaunal99@users.noreply.github.com>
2020-11-08 02:42:00 +0300

    Add files via upload

---
+++ login and registration/register/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
